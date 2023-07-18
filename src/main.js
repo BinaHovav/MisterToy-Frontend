e8e2d522a4a129e8927a2c43ea3db32b04125cd2
Bina Hovav <binahovav@gmail.com>
2023-07-18 21:51:56 +0300

    added elementplus and chart libs and fixed sortBy bug

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import { store } from './store'
 
 import App from './App.vue'
 import router from './router'
+import elementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.use(elementPlus)
 
 app.mount('#app')
